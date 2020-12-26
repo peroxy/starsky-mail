9cdff8ffcc7b8771080d0f8bb0f1cb0f31c81265
peroxy <peroxy@users.noreply.github.com>
2020-12-26 15:05:15 +0100

    Renamed StarskyMail.Api to StarskyMail.Queue.Api

---
+++ src/StarskyMail/StarskyMail.Queue.Api/Controllers/MailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StarskyMail.Queue.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MailController : ControllerBase
+    {
+        private readonly ILogger<MailController> _logger;
+
+        public MailController(ILogger<MailController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
--- src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
+++ src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace StarskyMail.Api
+namespace StarskyMail.Queue.Api
 {
     public class Startup
     {
@@ -27,7 +20,7 @@ namespace StarskyMail.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "StarskyMail.Api", Version = "v1"}); });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "StarskyMail Api", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,15 +30,12 @@ namespace StarskyMail.Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarskyMail.Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarskyMail Api v1"));
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
