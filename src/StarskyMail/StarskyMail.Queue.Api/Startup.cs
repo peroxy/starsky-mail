e9b31098cf4068fc0b0b098e6178855585cd6384
peroxy <peroxy@users.noreply.github.com>
2020-12-28 16:39:15 +0100

    API port set to 56789, added API to docker-compose, removed https (will not be public URL, no need)

--- src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
+++ src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
@@ -38,7 +38,6 @@ namespace StarskyMail.Queue.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarskyMail Api v1"));
             }
 
-            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
