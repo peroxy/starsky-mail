c09ea43a8038cc5f9920510d6445f4033fad14fd
peroxy <peroxy@users.noreply.github.com>
2020-12-29 18:43:48 +0100

    Added rabbitmq consumer with invitations worker

---
+++ src/StarskyMail/StarskyMail.Queue.Consumer/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace StarskyMail.Queue.Consumer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions<RabbitMQSettings>()
+                        .Bind(hostContext.Configuration.GetSection(RabbitMQSettings.Section))
+                        .ValidateDataAnnotations();
+            
+                    services.AddSingleton<QueueConfiguration>();
+                    services.AddHostedService<InvitationsWorker>();
+                });
+    }
+}
