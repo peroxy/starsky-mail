04af9ba37a3db19898061062a6108b5bbe283997
peroxy <peroxy@users.noreply.github.com>
2021-08-07 11:58:38 +0200

    Allow CORS requests

--- src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
+++ src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
@@ -36,6 +36,8 @@ namespace StarskyMail.Queue.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
