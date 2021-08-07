645856072fe605f9e03bbc31efb06a7d0b2c6833
peroxy <peroxy@users.noreply.github.com>
2021-08-07 12:44:11 +0200

    Fix CORS

--- src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
+++ src/StarskyMail/StarskyMail.Queue.Api/Startup.cs
@@ -21,6 +21,8 @@ namespace StarskyMail.Queue.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            
             services.AddOptions<RabbitMQSettings>()
                 .Bind(Configuration.GetSection(RabbitMQSettings.Section))
                 .ValidateDataAnnotations();
